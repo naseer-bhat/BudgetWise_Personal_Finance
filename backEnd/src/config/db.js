6251665e8efd7ad1e4b744e9cfb6a499a17ab1d3
naseer-bhat <studymail2k18@gmailo.com>
2025-04-23 23:05:47 +0530

    debuging

--- backEnd/src/config/db.js
+++ backEnd/src/config/db.js
@@ -1,10 +1,14 @@
-import {connect} from "mongoose";
+import { connect } from "mongoose";
+
 export const connectToDB = async (dbUrl) => {
-try {
-  await connect(dbUrl)
-  console.log("Connected to MongoDB");
-} catch (error) {
-  console.error("Error connecting to MongoDB:", error.message);
-  process.exit(1)
-}
-}
+  try {
+    await connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
